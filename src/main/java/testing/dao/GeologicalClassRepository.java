b5082b78d5d0a9a8746a82bd945cd891e0eaac86
prUnikitty <82715439+prUnikitty@users.noreply.github.com>
2021-04-27 12:29:48 +0300

    Delete src directory

--- src/main/java/testing/dao/GeologicalClassRepository.java
+++
@@ -1,8 +0,0 @@
-package testing.dao;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GeologicalClassRepository extends CrudRepository<GeologicalClass, Integer> {
-    List<GeologicalClass> findByName(String name);
-}
