1ae22267fd0d39fd49ed408acc46338c4694f43d
Markov Maxim <markov.me@mail.ru>
2021-06-03 16:16:51 +0300

    fixing tests for Section + async annotation

--- src/main/java/testing/Application.java
+++ src/main/java/testing/Application.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableAsync
 
 public class Application {
     public static void main(String[] args) {
